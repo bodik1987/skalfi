d7b62b5d3a52dbf6799b8fe3e0e6df26fea6cc82
bogdan shulika <bsshul@gmail.com>
2025-10-05 20:26:37 +0200

    PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Skalfi_bohdan",
+        short_name: "Skalfi_bohdan",
+        description: "Skalfi_bohdan",
+        theme_color: "#36373c",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
